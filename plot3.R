bb6a77eb87f1bf0a71c18da9b8a0e57f9a49b710
naonao85cz <75863538@qq.com>
2017-02-11 21:28:27 +0800

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -22,6 +22,6 @@ with(datasub,{
         axis(1,at=weekday,labels=weekday)
         lines(datetime,Sub_metering_2,,col="red")
         lines(datetime,Sub_metering_3,,col="blue")
-        legend("topright",pch="__",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+        legend("topright",lty=c(1,1),col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 })
 dev.off()
--- plot4.R
+++ plot4.R
@@ -29,7 +29,7 @@ with(datasub,{
         axis(1,at=weekday,labels=weekday)
         lines(datetime,Sub_metering_2,,col="red")
         lines(datetime,Sub_metering_3,,col="blue")
-        legend("topright",pch="__",col=c("black","red","blue"),,bty="n",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+        legend("topright",lty=c(1,1),col=c("black","red","blue"),,bty="n",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
         plot(datetime,Global_reactive_power,type="l",xlab="datetime")
         axis(1,at=weekday,labels=weekday)
 })
